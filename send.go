721a179a954ecc1dbbaf33d3dc947258940ae4c4
jeffotoni <jeff.otoni@s3wf.com.br>
2023-09-02 01:54:22 -0300

    new method send email

--- send.go
+++ send.go
@@ -4,6 +4,29 @@ import (
 	"errors"
 )
 
+type Config struct {
+	AWS_REGION            string `json:"AWS_REGION,omitempty"`
+	AWS_IDENTITY          string `json:"AWS_IDENTITY,omitempty"`
+	AWS_ACCESS_KEY_ID     string `json:"AWS_ACCESS_KEY_ID,omitempty"`
+	AWS_SECRET_ACCESS_KEY string `json:"AWS_SECRET_ACCESS_KEY,omitempty"`
+	AWS_FROM              string `json:"AWS_FROM,omitempty"`
+	AWS_MSG               string `json:"AWS_MSG,omitempty"`
+}
+
+func New(AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_FROM, AWS_MSG string) *Config {
+	return &Config{
+		AWS_REGION:            AWS_REGION,
+		AWS_ACCESS_KEY_ID:     AWS_ACCESS_KEY_ID,
+		AWS_SECRET_ACCESS_KEY: AWS_SECRET_ACCESS_KEY,
+		AWS_FROM:              AWS_FROM,
+		AWS_MSG:               AWS_MSG,
+	}
+}
+
+func (c *Config) SendEmailNew(To, From, FromMsg, Titulo, MsgHTML string) (err error) {
+	return SendEmail(To, From, FromMsg, Titulo, MsgHTML)
+}
+
 // SendEmail ..
 func SendEmail(To, From, FromMsg, Titulo, MsgHTML string) (err error) {
 	if len(To) == 0 {
