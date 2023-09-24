e1b116d17b8acb45c5300fb53236f193f8cfd78a
aclgo <marcellosanttos2014@gmail.com>
2023-09-24 15:19:12 -0300

    remove duplicate file conteins values types equal and create function to remove repetitions

--- v2/errors.go
+++ v2/errors.go
@@ -1,8 +1,20 @@
 package sesv2
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
+
+func fErr(param string) error {
+	return fmt.Errorf(`parameter "%v" is required.`, param)
+}
 
 var (
 	ErrNoProfileSet       = errors.New("no profile set")
 	ErrProfileNotSearched = errors.New("profile no exists")
+	ErrInvalidTo          = fErr("to")
+	ErrInvalidFrom        = fErr("from")
+	ErrInvalidMessage     = fErr("message")
+	ErrInvalidTitle       = fErr("to")
+	ErrInvalidMessageHTML = fErr("messageHtml")
 )
